33288d38c6ed7b3af7a641c52dc6f1e0c8b9557c
Alexander Le <alextle0125@gmail.com>
2014-08-16 17:28:01 -0400

    started project templating feature on main page

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,6 +1,6 @@
 class Result < ActiveRecord::Base
    validates :topic, :presence => true
-   validates :user_id, :presence => true
+   # validates :user_id, :presence => true
     validates :file_data, :presence => true
 
     belongs_to :user
