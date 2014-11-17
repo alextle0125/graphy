edebbf919b9663856f43ceabc871e796f8ff77aa
Alex Le <alextle0125@gmail.com>
2014-11-16 16:46:34 -0800

    new api key

--- app/helpers/fetch.rb
+++ app/helpers/fetch.rb
@@ -3,7 +3,7 @@ require "uri"
 
 helpers do
   def get_fda_data(query)
-    query_root = 'https://api.fda.gov/food/enforcement.json?api_key=ZObiWB8cMRYRRfPNnf41BGCLvXViUbNfTw6zHODr&search=reason_for_recall:';
+    query_root = 'https://api.fda.gov/food/enforcement.json?api_key=XajdC2VfR2I2IMvZFabHpZGG7z5v17Sii1Jg62JF&search=reason_for_recall:';
     query_suffix = '&count=report_date'
     uri_string = URI.escape(query_root + '"' + query + '"' + query_suffix)
     uri = URI.parse(uri_string)
